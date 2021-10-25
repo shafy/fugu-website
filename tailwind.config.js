4c8acfe257504e377fa859573a2b0493f0648b52
Can Olcer <canolcer@hey.com>
2021-10-25 15:25:37 +0000

    added tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  purge: {
+    enabled: process.env.HUGO_ENVIRONMENT === "production",
+    content: ["./layouts/**/*.html", "./content/**/*.md", "./content/**/*.html"],
+  },
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
